7b2038dd1bd07df7121e553b4e20c9287efa2f88
Sang Wok Lee <sangwoklee@192.168.1.100>
2023-03-13 17:32:06 +0000

    Refactored code

--- lambda/get_jobs_function/index.js
+++ lambda/get_jobs_function/index.js
@@ -1,7 +1,21 @@
 export const handler = async(event) => {
+    const postcodes = ['SW1A 0AA', 'NW1 4RY', 'E1 6BJ', 'SE1 7TP', 'W1D 7EJ'];
+    const providers = ['DHL', 'FedEx', 'UPS', 'TNT'];
+
+    const jobList = [];
+
+    for (let i = 0; i < 20; i++) {
+    const job = {
+        origin: postcodes[Math.floor(Math.random() * postcodes.length)],
+        destination: postcodes[Math.floor(Math.random() * postcodes.length)],
+        provider: providers[Math.floor(Math.random() * providers.length)]
+    };
+    jobList.push(job);
+    }
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: jobList,
     };
     return response;
 };
